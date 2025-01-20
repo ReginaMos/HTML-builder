d281f29ecdf18db532751ef2fc358e34f552eb80
Regina Moiseeva <moiseevaryu@gmail.com>
2025-01-20 09:49:37 +0500

    feat: add solution to 04 task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,49 @@
+const { log } = require('console');
+const fs = require('fs');
+const path = require("path");
+
+const dirPath = path.join(__dirname, 'files');
+const copyDirPath = path.join(__dirname, 'files-copy');
+
+fs.stat(copyDirPath, (err, stats) => {
+    if (err) {
+        fs.mkdir(copyDirPath, (err) => {
+            if (err) console.log(err);
+        });
+    }
+
+    fs.readdir(dirPath,
+        { withFileTypes: true },
+        (err, files) => {
+            if (err) console.log(err);
+            else {
+                files.forEach(file => {
+                    const filePath = path.join(dirPath, file.name);
+                    fs.copyFile(filePath, path.join(copyDirPath, file.name), (err) => {
+                        if (err) console.log(err);
+                    })
+                });
+            }
+      });
+});
+
+fs.readdir(copyDirPath, { withFileTypes: true }, (err, files) => {
+    if (err) console.log(err);
+    else {
+        files.forEach(file => {
+            const filePath = path.join(dirPath, file.name);
+            fs.stat(filePath, (err, stats) => {
+                if (err) {
+                    const deleteFilePath = path.join(copyDirPath, file.name);
+                    fs.unlink(deleteFilePath, (err) => {
+                        if (err) console.log(err);
+                    });
+                }
+            })
+        });
+    }
+  });
+
+
+
+
